7b01e6014a0fb84bae003ba0e372f74f982a0c12
ASHWIN <email@example.com>
2026-05-06 14:42:41 +0530

    Add

--- exercise1.py
+++ exercise1.py
@@ -1,2 +1,32 @@
 """1.Write a python program to initiate variable & print its data types. 
 2. Do Type Conversion for a) int to string b) string to int c) bool to string d) string to bool"""
+"""
+# 1 Problem
+name = "Annees"
+role = "Programmer"
+salary = 50000
+print(name,role,salary)
+print(type(name),type(role),type(salary))
+"""
+
+#. Problem
+from os import name
+
+
+mynum="100"
+print(mynum)
+print(type(mynum))
+mynum=int(mynum)
+print(mynum)
+print(type(mynum))
+
+#ame="Anees"
+#ame=int(name)
+#rint (name)
+
+phonenumber = 78549674890
+phonenumber = str(phonenumber)
+print(phonenumber)
+print(type(phonenumber))
+
+      
