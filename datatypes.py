e312953f420731ab9a03f7963d814416b47c5adf
ASHWIN <email@example.com>
2026-05-06 14:22:25 +0530

    Add

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+firstName="GUVI"
+since=2014
+print(firstName,since)
+print(type(firstName),type(since))
+
+
+mylist = [1,2,3,]
+print(mylist)
+print(type(mylist))
+
+myset = {1,2,3}
+print(mylist)
+print(type(mylist))
+
+tup=(1,2,3)
+print(type(tup))
+
+mydict={"name":"Guvi","since":2014
+        }
+print(mydict)
+print(type(mydict))
+
+mybool = True
+mybool2= False
+print(mybool)
+print(mybool2)
+print(type(mybool))
