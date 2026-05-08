4b634b4e0acfdc32b03a7ecce0452134c15f7868
ASHWIN <email@example.com>
2026-05-08 12:30:42 +0530

    Add File

--- Day2/string.py
+++ Day2/string.py
@@ -1,20 +1,23 @@
-mystr="Welcome to day2 Session on Python"
+mystr = "Welcome to day2 Session on Python"
 print(mystr)
-mystr='''
+
+# Inbuilt method
+print("Uppercase", mystr.upper())
+print("Lowercase", mystr.lower())
+print("Index", mystr.index('day'))
+
+# New multiline string
+mystr = '''
 i am a good boy
 i am an coder
-
 '''
+
 print(mystr)
+
 # Do not use in single quotes
 print(type(mystr))
 
-# Inbuilt method
-print("Uppercase",mystr.upper())
-print("Lowercase",mystr.lower())
-print("Index",mystr.index('day'))
-
-# Index always start from zero not other 
-print('capitalizemystr',mystr.capitalize())
+# Index always start from zero
+print('capitalizemystr', mystr.capitalize())
 print(mystr.count('a'))
-print("count",mystr.count('o'))
+print("count", mystr.count('o'))
