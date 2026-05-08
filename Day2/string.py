d74423f5e3b3bf5755571793661236494fc7462b
ASHWIN <email@example.com>
2026-05-08 12:22:56 +0530

    Add Files

---
+++ Day2/string.py
@@ -0,0 +1,16 @@
+mystr="Welcome to day2 Session on Python"
+print(mystr)
+mystr='''
+i am a good boy
+i am an coder
+
+'''
+print(mystr)
+# Do not use in single quotes
+print(type(mystr))
+
+# Inbuilt method
+print("Uppercase",mystr.upper())
+print("Lowercase",mystr.lower())
+print("Index",mystr.index('day'))
+
