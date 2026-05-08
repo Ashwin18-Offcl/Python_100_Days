fdd293c85e00dc28726fea5abc5c39a23e148c4b
ASHWIN <email@example.com>
2026-05-08 15:35:40 +0530

    Add file

---
+++ Day2/input.py
@@ -0,0 +1,7 @@
+employeename = input("Enter the employee name:\n")
+employeesalary = int(input("Enter the employee salary:\n"))
+employeePhone = int(input("Enter the employee Phone:\n"))
+
+print("Employee Name is", employeename)
+print("Employee salary is", employeesalary)
+print("Employee Phone is", employeePhone)
